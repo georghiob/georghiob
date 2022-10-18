08745bac3bc64a2720b32d35d8155062d7c4fcf2
Georg Hiob <georg.hiob@st.oth-regensburg.de>
2022-10-18 18:39:15 +0200

    javascript update

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-document.getElementById("alpha").addEventListener("click", Scrolltop);
+var ScrollToTopId = document.getElementById("alpha");
+
+ScrollToTopId.addEventListener("click", Scrolltop);
 
 
 function Scrolltop(){
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+var ScrollToBottomId = document.getElementById("gamma");
+
+ScrollToBottomId.addEventListener("click", ScrollBottom);
+
+var ScrollToBottomToo = document.getElementById("delta");
+
+ScrollToBottomToo.addEventListener("click", ScrollBottom);
+
+function ScrollBottom(){
+    document.body.scrollTop = 2000;
+    document.documentElement.scrollTop = 2000;
+}
+
+var ScrollAboutMe = document.getElementById("beta");
+
+ScrollAboutMe.addEventListener("click", ScrollToAboutMe);
+
+function ScrollToAboutMe(){
+    document.body.scrollTop = 610;
+    document.documentElement.scrollTop = 610;
+}
+
 
 var MailButton = document.getElementById("eta");
 MailButton.addEventListener("click", MailPresenter);
